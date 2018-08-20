91978a1a3dacf1852d5f54480a39c4fa21e9e091
George Gooding <george.gooding@epinova.no>
2018-08-20 15:02:48 +0200

    Rewrite

---
+++ README.md
@@ -0,0 +1,24 @@
+﻿# globbed-webpack-entries-plugin
+
+[![license][license]][license-url]
+
+Provides a way to glob for entry files in Webpack `watch` and `non-watch` modes.
+
+## Usage
+
+```js
+import GlobbedEntriesPlugin from 'globbed-webpack-entries-plugin';
+ 
+// In your Webpack config:
+{
+   
+    entry: GlobbedEntriesPlugin.entries({
+            global : ['./Scripts/index.js', './Styles/main.scss']
+        }
+    )
+    
+    plugins: [
+        new GlobbedEntriesPlugin(),
+    ]    
+}
+```
---
+++ index.js
@@ -0,0 +1,59 @@
+﻿const glob = require('glob');
+const globBase = require('glob-base');
+
+let directories = [];
+
+class GlobEntries {
+    static entries(entryObject) {
+        return function () {
+            const entries = {};
+
+            for (let bundle in entryObject) {
+
+                if (entryObject.hasOwnProperty(bundle) && Array.isArray(entryObject[bundle])) {
+                    let files = [];
+
+                    entryObject[bundle].forEach(globString => {
+                        let globDirectory = globBase(globString).base;
+
+                        if (directories.indexOf(globDirectory) < 0) {
+                            directories.push(globDirectory);
+                        }
+
+                        files = files.concat(glob.sync(globString));
+                    });
+
+                    if (files.length) {
+                        entries[bundle] = files;
+                    }
+                }
+            }
+
+            return entries;
+        }
+    }
+
+    /**
+     * Install Plugin
+     * @param {Object} compiler
+     */
+    apply(compiler) {
+        if (compiler.hooks) {
+            compiler.hooks.afterCompile.tapAsync(this.constructor.name, this.afterCompile.bind(this));
+        }
+    }
+
+    /**
+     * After compiling, give webpack the globbed files
+     * @param {Object} compilation
+     * @param {Function} callback
+     */
+    afterCompile(compilation, callback) {
+        for (const directory of directories) {
+            compilation.contextDependencies.add(directory);
+        }
+        callback();
+    }
+}
+
+module.exports = GlobEntries;
